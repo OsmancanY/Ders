1e6c0a3dc021d9b0e0ac06743619d88a6af9aa30
OsmancanY <osmancanykselgazi06@gmail.com>
2023-12-26 10:34:27 +0300

    #

---
+++ OkulAppSube1BIL/frmOgrenciRemove.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace OkulAppSube1BIL
+{
+    partial class frmOgrenciRemove
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxOgrenciId = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OgrenciID";
+            // 
+            // textBoxOgrenciId
+            // 
+            this.textBoxOgrenciId.Location = new System.Drawing.Point(251, 139);
+            this.textBoxOgrenciId.Name = "textBoxOgrenciId";
+            this.textBoxOgrenciId.Size = new System.Drawing.Size(177, 22);
+            this.textBoxOgrenciId.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(279, 167);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Eminmisiniz?";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmOgrenciRemove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBoxOgrenciId);
+            this.Controls.Add(this.label1);
+            this.Name = "frmOgrenciRemove";
+            this.Text = "frmOgrenciRemove";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxOgrenciId;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ OkulAppSube1BIL/frmOgrenciRemove.cs
@@ -0,0 +1,31 @@
+﻿using OkulApp.BLL.Repositories.OgrenciRepository;
+using System;
+using System.Windows.Forms;
+
+namespace OkulAppSube1BIL
+{
+    public partial class frmOgrenciRemove : Form
+    {
+        readonly private OgrenciWriteRepository _ogrenciWriteRepository;
+        public frmOgrenciRemove(OgrenciWriteRepository ogrenciWriteRepository)
+        {
+            _ogrenciWriteRepository = ogrenciWriteRepository;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (radioButton1 != null)
+            {
+                try
+                {
+                    _ogrenciWriteRepository.RemoveAsync(textBoxOgrenciId.Text);
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+            }
+            else { MessageBox.Show("Eminmisiniz sorusu atlanamaz."); }
+
+        }
+    }
+}
